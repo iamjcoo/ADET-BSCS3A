95a51b6d378193f0638208b0751dcfaa68c43cea
Hyacinthe Marie <hymediado@my.cspc.edu.ph>
2024-10-17 12:27:02 +0800

    PS3 done

---
+++ Mediado_Hyacinthe/PS3/config.py
@@ -0,0 +1,5 @@
+class Config:
+    DB_HOST = 'localhost'  # Change if your DB host is different
+    DB_USER = 'root'  # Your MySQL username
+    DB_PASSWORD = ''  # Your MySQL password
+    DB_NAME = 'adet'  # Your database name
