bfe8cb7aae19ee22a151492c8954c5fc9428a163
Mark Bryan De Silva <madesilva@my.cspc.edu.ph>
2024-10-17 12:59:54 +0800

    ADDED PS4 to DESilva Folder

---
+++ DeSilva_MarkBryan/PS4/PS4_De-Silva.sql
@@ -0,0 +1,58 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: adet
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `adet_user`
+--
+
+DROP TABLE IF EXISTS `adet_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `adet_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `contact_number` varchar(20) DEFAULT NULL,
+  `email_address` varchar(100) NOT NULL,
+  `address` text DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email_address` (`email_address`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adet_user`
+--
+
+LOCK TABLES `adet_user` WRITE;
+/*!40000 ALTER TABLE `adet_user` DISABLE KEYS */;
+INSERT INTO `adet_user` VALUES (1,'rolando','laut','perina','09772613123','rolandoperina@gmail.com','san juan nabua','7956901f651c4a20163c20ffaa8225b0d2739c4ea8fbb46491dbfacff169da00'),(2,'Mark Bryan','Roquid','De Silva','0924559862','madesilva@my.cspc.edu.ph','San Antonio Poblacion','bcb15f821479b4d5772bd0ca866c00ad5f926e3580720659cc80d39c9d09802a'),(3,'Mark Bryan','Roquid','De Silva','0924559862','madesilvaa@my.cspc.edu.ph','San Antonio Poblacion','1e7b4c76a396af32b76ac6d16d27c2ebffc4eaa5bc92043b48fd8e061fd13d55');
+/*!40000 ALTER TABLE `adet_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-17 12:55:26
